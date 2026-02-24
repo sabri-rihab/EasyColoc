964ebb75f551a66f25e4f10d1beefc5510bff90e
sabri-rihab <rihabsabri21@gmail.com>
2026-02-24 12:11:09 +0000

    instalation de breeze

--- easyColoc/database/migrations/0001_01_01_000000_create_users_table.php
+++ easyColoc/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('reputation');
+            $table->boolean('is_global_admin')->default(false);
+            $table->boolean('is_banned')->default(false);
+            $table->timestamp('banned_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
